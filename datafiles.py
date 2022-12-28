37a43eb95dc7c8c0e0a3dc0b99d42ebe63c7b853
baron070981 <baron070981@gmail.com>
2022-12-28 23:05:12 +0300

    небольшие изминения

--- datafiles.py
+++ datafiles.py
@@ -7,11 +7,6 @@ import re
 import os.path
 from pathlib import Path
 
-# создание б.д. путь к исходн. переименовывание исходников, создание копий с новыми именами по новому пути
-# 
-# 
-# path/to/db/name.id.count.jpg
-# 
 
 
 def gcd(ret_type=None):
@@ -20,40 +15,12 @@ def gcd(ret_type=None):
     return str(Path(__file__).resolve().parent)
 
 
-# def sort_files(path_to_files = None, ext_list = ['.jpg']):
-    # home = os.getcwd()
-    # os.chdir(path_to_files)
-    # file_list = list()
-    # for filename in os.listdir(os.getcwd()):
-        # for ext in ext_list:
-            # if filename.endswith(ext):
-                # file_list.append(os.path.abspath(filename))
-    # os.chdir(home)
-    # return file_list
-
-
-# была sort_files
 def filter_files(path_to_files, *exts):
     # выбирает из заданной директории файлы с задаными расширениями
     path_to_files = Path(path_to_files)
     file_list = path_to_files.iterdir()
     file_list = list(filter(lambda x: x.suffix in exts, file_list))
     return file_list
-
-
-
-
-# def rename_files(path_to_files, newname = None, ids = None):
-    # count = 0
-    # home = os.getcwd()
-    # os.chdir(path_to_files)
-    # temp_list = os.listdir(os.getcwd())
-    # for name in temp_list:
-        # if name.endswith('.jpg'):
-            # new_name = newname+'.'+str(ids)+'.'+str(count)+'.jpg'
-            # os.rename(name, new_name)
-            # count += 1
-    # os.chdir(home)
 
 
 def rename_file(path_to_file, idf, newname, count, *exts):
@@ -75,27 +42,6 @@ def rename_files(path_to_files, idf, newname, *exts):
         count += 1
 
 
-def create_new_db(db_name = None, path_db = os.getcwd(), files_path = None, name = None, idd = 0):
-    count = 0
-    file_list = list()
-    home = os.getcwd()
-    os.chdir(path_db)
-    os.makedirs(db_name, exist_ok = True)
-    os.chdir(db_name)
-    p = os.getcwd()
-    if files_path == None or name == None or idd == 0:
-        print('Создана пустая папка для БД.')
-        return False
-    temp_list = os.listdir(files_path)
-    for data in temp_list:
-        if data.endswith('.jpg'):
-            new_name = name+'.'+str(idd)+'.'+str(count)+'.jpg'
-            shutil.copyfile(files_path+'/'+data, p+'/'+new_name)
-            count += 1
-    os.chdir(home)
-    return True
-
-
 def is_valid_name(string):
     # проверка соответствия имени файла
     sp = string.split('_')
@@ -108,11 +54,10 @@ def is_valid_name(string):
 
 
 # была create_new_db
-def create_dir_for_datas(path_db, db_name, **kwargs):
+def create_dir_for_datas(path_db, dir_name, **kwargs):
     # создание 
     count = 0
-    path_db = Path(path_db) / db_name
-    # path_db.mkdir(parents=True, exist_ok=True)
+    path_db = Path(path_db) / dir_name
     path_to_files = kwargs.get('path_to_files')
     if not path_to_files:
         return
@@ -125,7 +70,7 @@ def create_dir_for_datas(path_db, db_name, **kwargs):
     
 
 
-def add_to_db_new_files(src = None, path_to_db = None, new_name = None, idd = 0):
+def add_to_db_new_files(src, path_to_save, new_name, idd):
     '''
  src -------- путь к папке откуда беруться изображения
  path_to_db - путь к базе с контрольными изображениями
@@ -136,144 +81,22 @@ def add_to_db_new_files(src = None, path_to_db = None, new_name = None, idd = 0)
               если не существует, то создастся новое имя, если id еще нет в базе
               иначе завершиться с кодом -1
     '''
-    if src == None or path_to_db == None or new_name == None or idd == 0:
-        print(add_to_db_new_files, 'Не определен один или несколько параметров')
-        return -1
-    pattern  = r'(\w+)[\.,_](\d+)[\.,_](\d+).\w+'
-    # name: [ids, counts]
-    list_db  = list()
-    list_src = list()
     
-    ids      = set()
-    counts   = list()
-    names    = dict()
-    
-    temp_id = idd
-    temp_count = 0
-    
-    list_db = os.listdir(path_to_db)
-    pprint(list_db)
-    home = os.getcwd()
-    for data in list_db:
-        if data.endswith('.jpg'):
-            data_split   = re.search(pattern, data)
-            _name        = data_split.group(1)
-            _id          = int(data_split.group(2))
-            ids.add(_id)
-            _count       = int(data_split.group(3))
-            names[_name] = [_id, _count]
-    print(names)
-    
-    for data in list_db:
-        if data.endswith('.jpg'):
-            data_split   = re.search(pattern, data)
-            _name        = data_split.group(1)
-            _count = int(data_split.group(3))
-            count = int(names[_name][1])
-            if count < _count:
-                names[_name][1] = _count
-    print(names)
-    
-    if new_name not in names and idd in ids:
-        print('Id уже существует для другого имени.')
-        return -1
-    if new_name in names:
-        temp_id = names[new_name][1]
-        temp_count = names[new_name][0]
-    
-    os.chdir(src)
-    temp_list = os.listdir(os.getcwd())
-    for name in temp_list:
-        if name.endswith('.jpg'):
-            newname = new_name+'.'+str(temp_id)+'.'+str(temp_count)+'.jpg'
-            shutil.copyfile(name, path_to_db+'/'+newname)
-            temp_count += 1
-    os.chdir(home)
     
 
-def get_params_from_db(path_db = None):
-    pattern  = r'(\w+)[\.,_](\d+)[\.,_](\d+).\w+'
-    file_list = os.listdir(path_db)
-    data_dict = dict()
-    for data in file_list:
-        if data.endswith('.jpg'):
-            data_split = re.search(pattern, data)
-            name = data_split.group(1)
-            idd = int(data_split.group(2))
-            data_dict[name] = idd
-    return data_dict
-    
-
-def get_list_names(path_db = None):
-    data_dict = get_params_from_db(path_db)
-    print(data_dict)
-    list_names = ['None']*(len(data_dict)+1)
-    for name in data_dict:
-        list_names[data_dict[name]] = name
-    return list_names
+def get_params(path_db = None):
+    ...
 
 
 def delete_data(path_to_db = None, dataname = None, idd = 0):
-    pattern = r'(\w+)[\.,_](\d+)[\.,_](\d+).\w+'
-    home = '/'.join(sys.argv[0].strip().split('/')[:-1])
-    os.chdir(path_to_db)
-    file_list = os.listdir(os.getcwd())
-    name = ''
-    _id = 0
-    if dataname == None and idd == 0:
-        print('Не введены данные для удаления.')
-        return 0
-    if dataname != '' and dataname != None:
-        count = 0
-        for _name in file_list:
-            print(_name)
-            name_split = re.search(pattern, _name)
-            if name_split == None:
-                continue
-            name = name_split.group(1)
-            if name == dataname:
-                count += 1
-                os.remove(_name)
-                print('Удаление ', _name)
-        print('Удаленно ', count, ' файлов')
-        return 0
-    else:
-        count = 0
-        for _name in file_list:
-            print(_name)
-            name_split = re.search(pattern, _name)
-            if name_split != None:
-                _id = int(name_split.group(2))
-                if _id == idd:
-                    count += 1
-                    os.remove(_name)
-                    print('Удаление ', _name)
-        print('Удаленно ', count, ' файлов')
-        return 0
-
-
-
-def show_database(path_to_db = None):
-    data_dict = get_params_from_db(path_to_db)
-    file_list = os.listdir(path_to_db)
-    count = 0
-    for data in data_dict:
-        count += 1
-        print(str(count)+'.', data, ':', data_dict[data])
-    for _file in file_list:
-        if not _file.endswith('.jpg'):
-            count += 1
-            print(str(count)+'.', _file)
+    ...
 
 
 
 if __name__ == '__main__':
     
     ...
-    p = Path('testdir2')
-    create_dir_for_datas('/home/baron/Coding/MyPython/toportcode/', 
-                         'testdir2', 
-                         path_to_files='/home/baron/Coding/MyPython/toportcode/testdir/')
+    
     
     
     
--- scaner_face.py
+++ scaner_face.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 import cv2
-import os
-import sys
 import numpy as np
-from PIL import Image
 from rich import print, inspect
 from pathlib import Path
 
@@ -68,8 +65,7 @@ def find_all_faces(path_to_imgs,
     if not file_list:
         print('Искомых файлов в директории не найдено.')
         return
-    
-    cascade_class = cv2.CascadeClassifier(cascade)
+    cascade_class = cv2.CascadeClassifier(str(cascade))
     
     for img_name in file_list:
         original = cv2.imread(str(img_name))
@@ -86,67 +82,12 @@ def find_all_faces(path_to_imgs,
         for (x,y,w,h) in faces:
             count += 1
             CNT += 1
-            face_img = original[y:y+h, x:x+w]
+            face_img = gray[y:y+h, x:x+w]
             if face_img.shape[0] < min_out_size:
                 face_img = cv2.resize(face_img, (min_out_size, min_out_size), cv2.INTER_AREA)
             name = path_to_save / f'{CNT}_{count}{img_name.suffix}'
             cv2.imwrite(str(name), face_img)
             print('Saving ', name)
-
-
-def find_person_face(path_to_img, path_to_save, path_to_contr, confid = 90.0,
-                     cascade = 'haarcascade_frontalface_default.xml'):
-    label_id = 0
-    ids = list()
-    datas = list()
-    contr_list = datafiles.filter_files(path_to_contr, '.jpg', '.png')
-    os.makedirs(path_to_save, exist_ok=True)
-    file_list = datafiles.filter_files(path_to_img, '.jpg', '.png')
-    face_cascade = None
-    resize_const = 800
-    
-    if not file_list:
-        print('Искомых файлов в директории не найдено.')
-        return
-    
-    face_cascade = cv2.CascadeClassifier(cascade)
-    
-    for name_img in contr_list:
-        original_contr = cv2.imread(str(name_img))
-        gray = cv2.cvtColor(original_contr, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,minSize=(30,30))
-        for (x,y,w,h) in faces:
-            datas.append(gray[y:y+h, x:x+w])
-            ids.append(label_id)
-            label_id += 1
-    
-    recognizer  = cv2.face.LBPHFaceRecognizer_create()
-    recognizer.train(datas, np.array(ids))
-    
-    for img_name in file_list:
-        original = cv2.imread(str(img_name))
-        s = original.shape
-        ys = s[0] 
-        xs = s[1] 
-        if xs > resize_const and xs > ys:
-            dim = xs / resize_const
-            xs = resize_const
-            ys = int(ys / dim)
-        elif ys > resize_const and ys > xs:
-            dim = ys / resize_const
-            ys = resize_const
-            xs = int(xs / dim)
-        else:
-            pass
-        original = cv2.resize(original, (xs, ys), cv2.INTER_AREA)
-        gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4,minSize=(30,30))
-        for (x,y,w,h) in faces:
-            num, conf = recognizer.predict(gray[y:y+h, x:x+w])
-            print('Num:', num, '  Conf:', round(conf, 5))
-            if conf <= confid:
-                cv2.imwrite(path_to_save+'/'+'person_'+os.path.basename(img_name), original[y:y+h, x:x+w])
-                print('Saving '+'person_'+os.path.basename(img_name), 'to', path_to_save)
 
 
 def trains(path_to_imgs, filename_train = None):
@@ -186,7 +127,7 @@ def camera_scaner(path_to_db = None, cascade = 'haarcascade_frontalface_default.
     label = ''
     names = datafiles.get_list_names(path_to_db)
     num = 0
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(-1)
     #cap.set(cv2.CAP_PROP_FPS, 10)
     cv2.namedWindow('video')
     while True:
@@ -197,7 +138,7 @@ def camera_scaner(path_to_db = None, cascade = 'haarcascade_frontalface_default.
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.35, minNeighbors=4,minSize=(30,30))
         
         for (x,y,w,h) in faces:
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (255,100,25), 3)
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (255,100,25), 1)
             
             num, conf = recognizer.predict(gray[y:y+h, x:x+w])
             if conf < 100:
@@ -208,8 +149,8 @@ def camera_scaner(path_to_db = None, cascade = 'haarcascade_frontalface_default.
                 label = "unknown"
                 info = "  {0}%".format(round(100 - conf))
         
-            cv2.putText(frame, str(label), (x + 5, y - 5), font, 1, (25, 55, 255), 3)
-            cv2.putText(frame, str(info), (x + 5, y + h + 30), font, 1, (5, 5, 255), 3)
+            cv2.putText(frame, str(label), (x + 5, y - 5), font, 1, (25, 55, 255), 2)
+            cv2.putText(frame, str(info), (x + 5, y + h + 30), font, 1, (5, 5, 255), 2)
         cv2.imshow('video', frame)
         key = cv2.waitKey(1)
         if key == 27 or key == 13:
@@ -228,21 +169,7 @@ def camera_scaner(path_to_db = None, cascade = 'haarcascade_frontalface_default.
 if __name__ == "__main__":
     ...
     
-    # inspect(inspect, all=True)
-    # find_all_faces('/home/baron/images/DCIM/ya/', path_to_save='./ya', scaleFactor=1.3)
-    # datafiles.rename_files('/home/baron/Coding/MyPython/faces_recogn/ya/', 1, 'baron', '.jpg', '.png')
-    # trains('/home/baron/Coding/MyPython/faces_recogn/ya/', 'train.yml')
     
-    camera_scaner('/home/baron/Coding/MyPython/faces_recogn/ya/')
-    
-    # origin = cv2.imread('/home/baron/images/DCIM/priroda/IMG_20220110_120651.jpg')
-    # xs, ys = get_modified_size(*origin.shape[:2], 600)
-    # origin = cv2.resize(origin, (xs, ys), cv2.INTER_AREA)
-    # cv2.imshow('test', origin)
-    # cv2.waitKey(0)
-
-
-
 
 
 
